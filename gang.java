79553e4a042133fcd2f73bfaaaebec14ca76e75a
Tsvetan-N <48643662+Tsvetan-N@users.noreply.github.com>
2019-05-03 10:39:33 +0200

    Add files via upload

--- gang.java
+++ gang.java
@@ -25,38 +25,41 @@ public class gang extends Application{
 	int zoommax=0;
 	double diameter=3;
 	ArrayList<Sphere> planets = new ArrayList<Sphere>();
-	
+
 	@Override
 	public void start(Stage primaryStage) {
 
 		makePlanets();
 
 		Polygon rocket = new Polygon();
-		double xModule = offX-diameter/2+s.planetaryObjects.get(4).x*scale;
-		double yModule = offY-diameter/2+s.planetaryObjects.get(4).y*scale;
-		double xModuleLeft = offX-diameter/2+s.planetaryObjects.get(4).x*scale+10;
-		double yModuleLeft = offY-diameter/2+s.planetaryObjects.get(4).y*scale+20;
-		double xModuleRight = offX-diameter/2+s.planetaryObjects.get(4).x*scale+20;
-		double yModuleRight = offY-diameter/2+s.planetaryObjects.get(4).y*scale+10;
+		double xModule = offX-diameter/2+s.planetaryObjects.get(22).x*scale;
+		double yModule = offY-diameter/2+s.planetaryObjects.get(22).y*scale;
+		double xModuleLeft = xModule+5;
+		double yModuleLeft = yModule+7;
+		double xModuleRight = xModule+7;
+		double yModuleRight = yModule+5;
 		System.out.println(yModuleLeft);
 		System.out.println(yModuleRight);
+		System.out.println(s.planetaryObjects.get(22).x + " " + s.planetaryObjects.get(22).y + " " + s.planetaryObjects.get(22).name);
 
 
-        rocket.getPoints().addAll(new Double[]{
-            xModule, yModule,
-            xModuleRight, yModuleRight,
-            xModuleLeft, yModuleLeft });
-        rocket.setFill(Color.RED);
+		rocket.getPoints().addAll(new Double[]{
+				xModule, yModule,
+				xModuleRight, yModuleRight,
+				xModuleLeft, yModuleLeft });
+		rocket.setFill(Color.RED);
 
-        rocket.setTranslateY(10);
+		rocket.setTranslateY(10);
 
 
 		Timeline timeline = new Timeline(
 				new KeyFrame(Duration.millis(20), t -> {
 					//Put here the code which is supposed to be repeated(or check the x,y coordinates and decide when to thrust)
+					s.updatePositions();
+					rocket.setTranslateX(offX-diameter/2+s.planetaryObjects.get(22).x*scale);
+					rocket.setTranslateY(offY-diameter/2+s.planetaryObjects.get(22).y*scale);
 					for(int j=0;j<planets.size();j++)
 					{
-						s.updatePositions();
 						planets.get(j).setTranslateX(offX-diameter/2+s.planetaryObjects.get(j).x*scale);
 						planets.get(j).setTranslateY(offY-diameter/2+s.planetaryObjects.get(j).y*scale);
 					}
@@ -74,26 +77,26 @@ public class gang extends Application{
 		scene.setFill(Color.BLACK);
 
 		Camera camera = new PerspectiveCamera(false);
-     	scene.setCamera(camera);
+		scene.setCamera(camera);
 
-     	camera.translateZProperty().set(-500);
-     	//camera.setNearClip(1);
-     	camera.setFarClip(5000);
+		camera.translateZProperty().set(-500);
+		//camera.setNearClip(1);
+		camera.setFarClip(5000);
 
-     	primaryStage.addEventHandler(KeyEvent.KEY_PRESSED, event -> {
-            switch (event.getCode()) {
-                case Q:
-                	if (zoommax<1930){
-                    	camera.translateZProperty().set(camera.getTranslateZ() + 500);
-                    	zoommax=zoommax+500;}
-                    break;
-                case A:
-                    camera.translateZProperty().set(camera.getTranslateZ() - 500);
-                    zoommax=zoommax-500;
-                    break;
-                case T:
-                	scale=10*Math.pow(10,-8);
-                	offX=600-(int)(diameter/2+s.planetaryObjects.get(11).x*scale);
+		primaryStage.addEventHandler(KeyEvent.KEY_PRESSED, event -> {
+			switch (event.getCode()) {
+				case Q:
+					if (zoommax<1930){
+						camera.translateZProperty().set(camera.getTranslateZ() + 500);
+						zoommax=zoommax+500;}
+					break;
+				case A:
+					camera.translateZProperty().set(camera.getTranslateZ() - 500);
+					zoommax=zoommax-500;
+					break;
+				case T:
+					scale=10*Math.pow(10,-8);
+					offX=600-(int)(diameter/2+s.planetaryObjects.get(11).x*scale);
 					offY=600-(int)(diameter/2+s.planetaryObjects.get(11).y*scale);
 					camera.translateZProperty().set(500);
 					oldOffX=offX;
@@ -101,47 +104,47 @@ public class gang extends Application{
 					break;
 
 				case S:
-                	scale=10*Math.pow(10,-10);
-                	offX=600;
+					scale=10*Math.pow(10,-10);
+					offX=600;
 					offY=600;
 					camera.translateZProperty().set(500);
 					oldOffX=offX;
 					oldOffY=offY;
 					break;
 				case E:
-                	scale=10*Math.pow(10,-8);
-                	offX=600-(int)(diameter/2+s.planetaryObjects.get(4).x*scale);
+					scale=10*Math.pow(10,-8);
+					offX=600-(int)(diameter/2+s.planetaryObjects.get(4).x*scale);
 					offY=600-(int)(diameter/2+s.planetaryObjects.get(4).y*scale);
 					camera.translateZProperty().set(500);
 					oldOffX=offX;
 					oldOffY=offY;
 					break;
 			}
-        });
+		});
 
 
-        primaryStage.addEventHandler(MouseEvent.MOUSE_PRESSED, event -> { 
-                fstClickX=(int)(event.getX());
-		 		fstClickY=(int)(event.getY());
-            
-        });
+		primaryStage.addEventHandler(MouseEvent.MOUSE_PRESSED, event -> {
+			fstClickX=(int)(event.getX());
+			fstClickY=(int)(event.getY());
 
-         primaryStage.addEventHandler(MouseEvent.MOUSE_RELEASED, event -> { 
-                oldOffX=offX;
-				oldOffY=offY;
-            
-        });
+		});
 
-         primaryStage.addEventHandler(MouseEvent.MOUSE_DRAGGED, event -> { 
-                offX=oldOffX+(int)(event.getX())-fstClickX;
-				offY=oldOffY+(int)(event.getY())-fstClickY;
-            
-        });
-     	
- 
-        primaryStage.setTitle("SolarSystem");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+		primaryStage.addEventHandler(MouseEvent.MOUSE_RELEASED, event -> {
+			oldOffX=offX;
+			oldOffY=offY;
+
+		});
+
+		primaryStage.addEventHandler(MouseEvent.MOUSE_DRAGGED, event -> {
+			offX=oldOffX+(int)(event.getX())-fstClickX;
+			offY=oldOffY+(int)(event.getY())-fstClickY;
+
+		});
+
+
+		primaryStage.setTitle("SolarSystem");
+		primaryStage.setScene(scene);
+		primaryStage.show();
 	}
 
 	public void makePlanets () {
@@ -150,17 +153,19 @@ public class gang extends Application{
 
 		for(Planet p : s.planetaryObjects)
 		{
-			Sphere planet = new Sphere();
-			planet.setTranslateX((int)(offX-diameter/2+p.x*scale));
-			planet.setTranslateY((int)(offY-diameter/2+p.y*scale));
-			planet.setRadius(diameter);
-			//System.out.println(planet.getCenterX() + " " +  planet.getCenterY());
+			if(p.name!="Lander") {
+				Sphere planet = new Sphere();
+				planet.setTranslateX((int) (offX - diameter / 2 + p.x * scale));
+				planet.setTranslateY((int) (offY - diameter / 2 + p.y * scale));
+				planet.setRadius(diameter);
+				//System.out.println(planet.getCenterX() + " " +  planet.getCenterY());
 
-			planets.add(planet);
+				planets.add(planet);
+			}
 		}
 	}
 
-	
+
 	public static void main(String[] args) {
 		launch(args);
 
