ef4b3c254dace9d2fd7366a3831dd0811e8c3332
Tsvetan-N <48643662+Tsvetan-N@users.noreply.github.com>
2019-05-07 12:42:12 +0200

    Add files via upload

--- src/gang.java
+++ src/gang.java
@@ -17,6 +17,9 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.input.MouseEvent;
 import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
+
+import static java.lang.Math.sqrt;
 
 public class gang extends Application{
 	int offX=600, offY=600;
@@ -27,7 +30,7 @@ public class gang extends Application{
 	int zoommax=0;
 	double diameter=10;
 	ArrayList<Sphere> planets = new ArrayList<Sphere>();
-	
+
 	@Override
 	public void start(Stage primaryStage) {
 
@@ -42,21 +45,32 @@ public class gang extends Application{
 		double yModuleRight = yModule-5;
 
 
-        rocket.getPoints().addAll(new Double[]{
-            xModule, yModule,
-            xModuleRight, yModuleRight,
-            xModuleLeft, yModuleLeft });
-        rocket.setFill(Color.RED);
-        System.out.println(xModule + " " + yModule);
+		rocket.getPoints().addAll(new Double[]{
+				xModule, yModule,
+				xModuleRight, yModuleRight,
+				xModuleLeft, yModuleLeft });
+		rocket.setFill(Color.RED);
 
 		Timeline timeline = new Timeline(
 				new KeyFrame(Duration.millis(2), t -> {
 					//Put here the code which is supposed to be repeated(or check the x,y coordinates and decide when to thrust)
-
+					if(sqrt((s.planetaryObjects.get(1).y-s.planetaryObjects.get(0).y)*(s.planetaryObjects.get(1).y-s.planetaryObjects.get(0).y)+
+							(s.planetaryObjects.get(1).x-s.planetaryObjects.get(0).x)*(s.planetaryObjects.get(1).x-s.planetaryObjects.get(0).x))<=2500000)
+					{
+						System.out.println(s.planetaryObjects.get(1).velX + " " + s.planetaryObjects.get(1).velY);
+						System.out.println(s.planetaryObjects.get(1).x + " " + s.planetaryObjects.get(1).y);
+						try
+						{
+							Thread.sleep(20000);
+						}
+						catch(InterruptedException ex)
+						{
+							Thread.currentThread().interrupt();
+						}
+					}
 					s.updatePositions();
 
 					rocket.setTranslateX(s.planetaryObjects.get(1).x*scale);
-					//test
 					rocket.setTranslateY(s.planetaryObjects.get(1).y*scale);
 
 					for(int j=0;j<planets.size();j++)
@@ -79,11 +93,11 @@ public class gang extends Application{
 
 		Camera camera = new PerspectiveCamera(false);
 
-     	scene.setCamera(camera);
+		scene.setCamera(camera);
 
-     	camera.translateZProperty().set(-500);
-     	//camera.setNearClip(1);
-     	camera.setFarClip(5000);
+		camera.translateZProperty().set(-500);
+		//camera.setNearClip(1);
+		camera.setFarClip(5000);
 
 		primaryStage.addEventHandler(KeyEvent.KEY_PRESSED, event -> {
 			switch (event.getCode()) {
@@ -105,40 +119,40 @@ public class gang extends Application{
 					break;
 
 				case S:
-                	scale=10*Math.pow(10,-10);
-                	camera.translateXProperty().set(0);
-                	camera.translateYProperty().set(0);
+					scale=10*Math.pow(10,-10);
+					camera.translateXProperty().set(0);
+					camera.translateYProperty().set(0);
 					scale=10*Math.pow(10,-10);
 					offX=600;
 					offY=600;
 					camera.translateZProperty().set(500);
 					break;
 				case E:
-                	scale=10*Math.pow(10,-8);
-                	camera.translateXProperty().set((int)(diameter/2+s.planetaryObjects.get(4).x*scale));
-                	camera.translateYProperty().set((int)(diameter/2+s.planetaryObjects.get(4).y*scale));
+					scale=10*Math.pow(10,-8);
+					camera.translateXProperty().set((int)(diameter/2+s.planetaryObjects.get(4).x*scale));
+					camera.translateYProperty().set((int)(diameter/2+s.planetaryObjects.get(4).y*scale));
 					scale=10*Math.pow(10,-8);
 					offX=600-(int)(diameter/2+s.planetaryObjects.get(4).x*scale);
 					offY=600-(int)(diameter/2+s.planetaryObjects.get(4).y*scale);
 					camera.translateZProperty().set(500);
 					break;
 			}
-        });
+		});
 
 
-        primaryStage.addEventHandler(MouseEvent.MOUSE_PRESSED, event -> { 
-                fstClickX=(int)(event.getX());
-                System.out.println(fstClickX);
-		 		fstClickY=(int)(event.getY());
-            
-        });
+		primaryStage.addEventHandler(MouseEvent.MOUSE_PRESSED, event -> {
+			fstClickX=(int)(event.getX());
+			System.out.println(fstClickX);
+			fstClickY=(int)(event.getY());
+
+		});
 
 
-         primaryStage.addEventHandler(MouseEvent.MOUSE_DRAGGED, event -> { 
-                camera.translateXProperty().set(-(((int)(event.getX())-camera.getTranslateX())-fstClickX));
-				camera.translateYProperty().set(-(((int)(event.getY())-camera.getTranslateY())-fstClickY));
-            
-        });
+		primaryStage.addEventHandler(MouseEvent.MOUSE_DRAGGED, event -> {
+			camera.translateXProperty().set(-(((int)(event.getX())-camera.getTranslateX())-fstClickX));
+			camera.translateYProperty().set(-(((int)(event.getY())-camera.getTranslateY())-fstClickY));
+
+		});
 
 		primaryStage.addEventHandler(MouseEvent.MOUSE_PRESSED, event -> {
 			fstClickX=(int)(event.getX());
@@ -170,27 +184,27 @@ public class gang extends Application{
 
 		for(Planet p : s.planetaryObjects)
 		{
-			//if(p.name!="Lander") {
-				Sphere planet = new Sphere();
-				planet.setTranslateX((int) (offX - diameter / 2 + p.x * scale));
-				planet.setTranslateY((int) (offY - diameter / 2 + p.y * scale));
+			if(p.name!="Lander") {
+			Sphere planet = new Sphere();
+			planet.setTranslateX((int) (offX - diameter / 2 + p.x * scale));
+			planet.setTranslateY((int) (offY - diameter / 2 + p.y * scale));
 
-				int a = (int) (offX - diameter / 2 + p.x * scale);
-				int b = (int) (offY - diameter / 2 + p.y * scale);
+			int a = (int) (offX - diameter / 2 + p.x * scale);
+			int b = (int) (offY - diameter / 2 + p.y * scale);
 
-				System.out.println(a + " " + b);
-				planet.setRadius(diameter);
+			System.out.println(a + " " + b);
+			planet.setRadius(diameter);
 
 
-				if (p.name == "centerTitan") {
+			if (p.name == "centerTitan") {
 
-					PhongMaterial material = new PhongMaterial();
-					material.setDiffuseMap(new Image("http://westciv.com/images/wdblogs/radialgradients/simpleclorstops.png"));
-					planet.setMaterial(material);
-				}
+				PhongMaterial material = new PhongMaterial();
+				material.setDiffuseMap(new Image("http://westciv.com/images/wdblogs/radialgradients/simpleclorstops.png"));
+				planet.setMaterial(material);
+			}
 
-				planets.add(planet);
-			//}
+			planets.add(planet);
+			}
 		}
 	}
 
